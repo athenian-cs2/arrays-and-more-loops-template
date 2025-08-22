d54b8cc71230bafcfba3deca16b87b98603e2c81
sthompson-athenian <sthompson@athenian.org>
2025-08-22 16:04:01 -0700

    Update Gradle to 7.6, Java to 17, and apply code formatting

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,30 +1,28 @@
-import java.util.Arrays;
 
 public class MyMain {
 
-    // Rotates all values in an array to the left
-    // Ex. rotateLeft([1, 2, 3, 4, 5]) => [2, 3, 4, 5, 1]
-    public static int[] rotateLeft(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
-    }
+  // Rotates all values in an array to the left
+  // Ex. rotateLeft([1, 2, 3, 4, 5]) => [2, 3, 4, 5, 1]
+  public static int[] rotateLeft(int[] arr) {
+    // REPLACE WITH YOUR CODE HERE
+    return null;
+  }
 
-    // Rotates all values in an array to the right
-    // Ex. rotateRight([1, 2, 3, 4, 5]) => [5, 1, 2, 3, 4]
-    public static int[] rotateRight(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
-    }
+  // Rotates all values in an array to the right
+  // Ex. rotateRight([1, 2, 3, 4, 5]) => [5, 1, 2, 3, 4]
+  public static int[] rotateRight(int[] arr) {
+    // REPLACE WITH YOUR CODE HERE
+    return null;
+  }
 
-    // Replaces all duplicate values with -1
-    // Ex. duplicateReplacer([1, 2, 1, 2, 1, 1]) => [1, 2, -1, -1, -1, -1]
-    public static int[] duplicateReplacer(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
-    }
+  // Replaces all duplicate values with -1
+  // Ex. duplicateReplacer([1, 2, 1, 2, 1, 1]) => [1, 2, -1, -1, -1, -1]
+  public static int[] duplicateReplacer(int[] arr) {
+    // REPLACE WITH YOUR CODE HERE
+    return null;
+  }
 
-
-    public static void main(String[] args) {
-        // YOUR CODE HERE
-    }
+  public static void main(String[] args) {
+    // YOUR CODE HERE
+  }
 }
--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,31 +1,70 @@
-import org.junit.jupiter.api.Test;
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
 
 public class MyTests {
 
-    @Test
-    public void testRotateLeft() {
-        assertEquals("[2, 3, 4, 5, 1]", Arrays.toString(MyMain.rotateLeft(new int[] {1, 2, 3, 4, 5})), "The array [1, 2, 3, 4, 5] when rotated left should be: " + "[2, 3, 4, 5, 1]");
-        assertEquals("[4, 3, 2, 1, 5]", Arrays.toString(MyMain.rotateLeft(new int[] {5, 4, 3, 2, 1})), "The array [5, 4, 3, 2, 1] when rotated left should be: " + "[4, 3, 2, 1, 5]");
-        assertEquals("[1, 1, 1, 1, 1]", Arrays.toString(MyMain.rotateLeft(new int[] {1, 1 ,1, 1, 1})), "The array [1, 1 ,1, 1, 1] when rotated left should be: " + "[1, 1, 1, 1, 1]");
-        assertEquals("[-1, 3, 5, 2]", Arrays.toString(MyMain.rotateLeft(new int[] {2, -1, 3, 5})), "The array [2, -1, 3, 5] when rotated left should be: " + "[-1, 3, 5, 2]");
-    }
+  @Test
+  public void testRotateLeft() {
+    assertEquals(
+        "[2, 3, 4, 5, 1]",
+        Arrays.toString(MyMain.rotateLeft(new int[] {1, 2, 3, 4, 5})),
+        "The array [1, 2, 3, 4, 5] when rotated left should be: " + "[2, 3, 4, 5, 1]");
+    assertEquals(
+        "[4, 3, 2, 1, 5]",
+        Arrays.toString(MyMain.rotateLeft(new int[] {5, 4, 3, 2, 1})),
+        "The array [5, 4, 3, 2, 1] when rotated left should be: " + "[4, 3, 2, 1, 5]");
+    assertEquals(
+        "[1, 1, 1, 1, 1]",
+        Arrays.toString(MyMain.rotateLeft(new int[] {1, 1, 1, 1, 1})),
+        "The array [1, 1 ,1, 1, 1] when rotated left should be: " + "[1, 1, 1, 1, 1]");
+    assertEquals(
+        "[-1, 3, 5, 2]",
+        Arrays.toString(MyMain.rotateLeft(new int[] {2, -1, 3, 5})),
+        "The array [2, -1, 3, 5] when rotated left should be: " + "[-1, 3, 5, 2]");
+  }
 
-    @Test
-    public void testRotateRight() {
-        assertEquals("[5, 1, 2, 3, 4]", Arrays.toString(MyMain.rotateRight(new int[] {1, 2, 3, 4, 5})), "The array [1, 2, 3, 4, 5] when rotated right should be: " + "[5, 1, 2, 3, 4]");
-        assertEquals("[1, 5, 4, 3, 2]", Arrays.toString(MyMain.rotateRight(new int[] {5, 4, 3, 2, 1})), "The array [5, 4, 3, 2, 1] when rotated right should be: " + "[1, 5, 4, 3, 2]");
-        assertEquals("[1, 1, 1, 1, 1]", Arrays.toString(MyMain.rotateRight(new int[] {1, 1 ,1, 1, 1})), "The array [1, 1 ,1, 1, 1] when rotated right should be: " + "[1, 1, 1, 1, 1]");
-        assertEquals("[5, 2, -1, 3]", Arrays.toString(MyMain.rotateRight(new int[] {2, -1, 3, 5})), "The array [2, -1, 3, 5] when rotated right should be: " + "[5, 2, -1, 3]");
-    }
+  @Test
+  public void testRotateRight() {
+    assertEquals(
+        "[5, 1, 2, 3, 4]",
+        Arrays.toString(MyMain.rotateRight(new int[] {1, 2, 3, 4, 5})),
+        "The array [1, 2, 3, 4, 5] when rotated right should be: " + "[5, 1, 2, 3, 4]");
+    assertEquals(
+        "[1, 5, 4, 3, 2]",
+        Arrays.toString(MyMain.rotateRight(new int[] {5, 4, 3, 2, 1})),
+        "The array [5, 4, 3, 2, 1] when rotated right should be: " + "[1, 5, 4, 3, 2]");
+    assertEquals(
+        "[1, 1, 1, 1, 1]",
+        Arrays.toString(MyMain.rotateRight(new int[] {1, 1, 1, 1, 1})),
+        "The array [1, 1 ,1, 1, 1] when rotated right should be: " + "[1, 1, 1, 1, 1]");
+    assertEquals(
+        "[5, 2, -1, 3]",
+        Arrays.toString(MyMain.rotateRight(new int[] {2, -1, 3, 5})),
+        "The array [2, -1, 3, 5] when rotated right should be: " + "[5, 2, -1, 3]");
+  }
 
-    @Test
-    public void testDuplicateReplacer() {
-        assertEquals("[1, 2, 3, 4, 5]", Arrays.toString(MyMain.duplicateReplacer(new int[] {1, 2, 3, 4, 5})), "The array [1, 2, 3, 4, 5] when duplicate replaced should be: " + "[1, 2, 3, 4, 5]");
-        assertEquals("[1, -1, -1, -1, -1]", Arrays.toString(MyMain.duplicateReplacer(new int[] {1, 1, 1, 1, 1})), "The array [1, 1, 1, 1, 1] when duplicate replaced should be: " + "[1, -1, -1, -1, -1]");
-        assertEquals("[1, 2, -1, -1, -1, -1]", Arrays.toString(MyMain.duplicateReplacer(new int[] {1, 2, 1, 2, 1, 1})), "The array [1, 2, 1, 2, 1, 1] when duplicate replaced should be: " + "[1, 2, -1, -1, -1, -1]");
-        assertEquals("[5, 1, 3, 2, -1, 6, 7, -1, -1, -1, 4, -1, -1]", Arrays.toString(MyMain.duplicateReplacer(new int[] {5, 1, 3, 2, 1, 6, 7, 2, 3, 1, 4, 2, 3})), "The array [5, 1, 3, 2, 1, 6, 7, 2, 3, 1, 4, 2, 3] when duplicate replaced should be: " + "[5, 1, 3, 2, -1, 6, 7, -1, -1, -1, 4, -1, -1]");
-    }
+  @Test
+  public void testDuplicateReplacer() {
+    assertEquals(
+        "[1, 2, 3, 4, 5]",
+        Arrays.toString(MyMain.duplicateReplacer(new int[] {1, 2, 3, 4, 5})),
+        "The array [1, 2, 3, 4, 5] when duplicate replaced should be: " + "[1, 2, 3, 4, 5]");
+    assertEquals(
+        "[1, -1, -1, -1, -1]",
+        Arrays.toString(MyMain.duplicateReplacer(new int[] {1, 1, 1, 1, 1})),
+        "The array [1, 1, 1, 1, 1] when duplicate replaced should be: " + "[1, -1, -1, -1, -1]");
+    assertEquals(
+        "[1, 2, -1, -1, -1, -1]",
+        Arrays.toString(MyMain.duplicateReplacer(new int[] {1, 2, 1, 2, 1, 1})),
+        "The array [1, 2, 1, 2, 1, 1] when duplicate replaced should be: "
+            + "[1, 2, -1, -1, -1, -1]");
+    assertEquals(
+        "[5, 1, 3, 2, -1, 6, 7, -1, -1, -1, 4, -1, -1]",
+        Arrays.toString(
+            MyMain.duplicateReplacer(new int[] {5, 1, 3, 2, 1, 6, 7, 2, 3, 1, 4, 2, 3})),
+        "The array [5, 1, 3, 2, 1, 6, 7, 2, 3, 1, 4, 2, 3] when duplicate replaced should be: "
+            + "[5, 1, 3, 2, -1, 6, 7, -1, -1, -1, 4, -1, -1]");
+  }
 }
